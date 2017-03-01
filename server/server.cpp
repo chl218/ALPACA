f13cda27675807c39572232ca9724b81bcb41d00
jack <chl218@eng.ucsd.edu>
2017-03-01 01:33:51 -0800

    added server to pi interface

--- server/server.cpp
+++ server/server.cpp
@@ -1,19 +1,86 @@
-#include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
+//-----------------------------------------------------------------------------
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 
+//-----------------------------------------------------------------------------
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+//-----------------------------------------------------------------------------
+#include <wiringPi.h>
+#include <softPwm.h>
+#include "interface.h"
+
+#define WIDTH      16
+#define HEIGHT     9
+#define NEWR_RANGE WIDTH * HEIGHT
+
+void initializePins() {
+   softPwmCreate(PIN_00, 0x00, 0xFF);
+   softPwmCreate(PIN_01, 0x00, 0xFF);
+   softPwmCreate(PIN_02, 0x00, 0xFF);
+   softPwmCreate(PIN_03, 0x00, 0xFF);
+   softPwmCreate(PIN_04, 0x00, 0xFF);
+   softPwmCreate(PIN_05, 0x00, 0xFF);
+   softPwmCreate(PIN_06, 0x00, 0xFF);
+   softPwmCreate(PIN_07, 0x00, 0xFF);
+   softPwmCreate(PIN_08, 0x00, 0xFF);
+   softPwmCreate(PIN_09, 0x00, 0xFF);
+   
+   softPwmCreate(PIN_10, 0x00, 0xFF);
+   softPwmCreate(PIN_11, 0x00, 0xFF);
+   softPwmCreate(PIN_12, 0x00, 0xFF);
+   softPwmCreate(PIN_13, 0x00, 0xFF);
+   softPwmCreate(PIN_14, 0x00, 0xFF);
+   softPwmCreate(PIN_15, 0x00, 0xFF);
+   softPwmCreate(PIN_16, 0x00, 0xFF);
+   softPwmCreate(PIN_17, 0x00, 0xFF);
+   softPwmCreate(PIN_18, 0x00, 0xFF);
+   softPwmCreate(PIN_19, 0x00, 0xFF);
+
+   softPwmCreate(PIN_20, 0x00, 0xFF);
+   softPwmCreate(PIN_21, 0x00, 0xFF);
+   softPwmCreate(PIN_22, 0x00, 0xFF);
+   softPwmCreate(PIN_23, 0x00, 0xFF);
+   softPwmCreate(PIN_24, 0x00, 0xFF);
+   softPwmCreate(PIN_25, 0x00, 0xFF);
+   softPwmCreate(PIN_26, 0x00, 0xFF);
+   softPwmCreate(PIN_27, 0x00, 0xFF);
+   softPwmCreate(PIN_28, 0x00, 0xFF);
+   softPwmCreate(PIN_29, 0x00, 0xFF);
+
+   softPwmCreate(PIN_30, 0x00, 0xFF);
+   softPwmCreate(PIN_31, 0x00, 0xFF);
+
+}
+
+int rangeTransform(int oldVal, int oldRange, int newRange) {
+   // OldRange = (OldMax - OldMin)  
+   // NewRange = (NewMax - NewMin)  
+   // NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin
+   return (((oldVal - 0) * newRange) / oldRange) + 0;
+}
+
+void drawLED(int xCrd, int yCrd, int gradient) {
+   softPwmWrite(xCrd * yCrd,  gradient);
+}
+
+
 int main( int argc, char *argv[] ) {
+
+   initializePins();
+
    int sockfd, portno; 
    socklen_t newsockfd;
    socklen_t clilen;
    char buffer[256];
    struct sockaddr_in serv_addr, cli_addr;
    int n;
+
    /* First call to socket() function */
    sockfd = socket(AF_INET, SOCK_STREAM, 0);
    if (sockfd < 0) {
@@ -44,7 +111,7 @@ int main( int argc, char *argv[] ) {
       /* Now start listening for the clients, here process will
        * go in sleep mode and will wait for the incoming connection
        */
-      listen(sockfd,5);
+      listen(sockfd, 5);
       clilen = sizeof(cli_addr);
       /* Accept actual connection from the client */
       newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
@@ -75,15 +142,21 @@ int main( int argc, char *argv[] ) {
       int screenWidth  = recv[0];
       int screenHeight = recv[1];
 
-      int xCrd = recv[2];
-      int yCrd = recv[3];
+      int xCrd = rangeTransform(recv[2], screenWidth,  WIDTH);
+      int yCrd = rangeTransform(recv[3], screenHeight, HEIGHT);
 
       int isOn     = recv[4];
-      int gradient = recv[5];
+      int gradient = rangeTransform(recv[5], 100, 0xFF);
+
+      printf("after rangeTransform: ");
+      for(int i = 0; i < 6; i++) {
+         printf("%d ", recv[i]);
+      }
+      printf("\n");
 
       /* Write a response to the client */
-      char msg[] = "I got your message\n";
-      n = write(newsockfd, msg, strlen(msg));
+      // char msg[] = "I got your message\n";
+      // n = write(newsockfd, msg, strlen(msg));
       if (n < 0) {
          perror("ERROR writing to socket");
          exit(1);
